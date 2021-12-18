6c99ffd3a68c663db03e14a597f3aa94ff1ce247
Adrian Tuzimek <tuziomek@gmail.com>
2021-12-18 15:48:12 +0100

    fix: enable cloning all repositories

--- src/github_backup/backup.py
+++ src/github_backup/backup.py
@@ -6,5 +6,5 @@ def do_backup():
     client = Client.from_env()
     client.login()
     repos = client.list_repos()
-    for repo in repos[:3]:
+    for repo in repos:
         client.clone_repo(repo)
